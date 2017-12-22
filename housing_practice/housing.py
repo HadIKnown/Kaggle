4e8aeef0f7825a5c236fc3dda6bf4fc6e62a8a14
HadiKnown <yeshar.hadi@gmail.com>
2017-12-21 23:37:40 -0500

    Removed several features which only explained an insignificant amount of variance in data. Only made minor improvements in performance. Still looking for the big leap that makes this implementation great rather than just good.

--- housing_practice/housing.py
+++ housing_practice/housing.py
@@ -1,18 +1,22 @@
 import pandas as pd
 import numpy as np
 from catboost import Pool, CatBoostRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import r2_score, make_scorer, mean_squared_error
+import math
 
 raw_data = pd.read_csv('train.csv') 
+print raw_data
 
 #to adjust final sale price prediction
 misc_values = raw_data['MiscVal']
 
 #Narrowed down to 20 features Dec 14
 
-def regularize(column):
-    return (column - np.mean(column)) / (max(column) - min(column))
+def regularize(column, dog = False):
+    reg_column = (column - np.mean(column)) / (max(column) - min(column))
+    #if dog: print reg_column.quantile(0.15), reg_column.quantile(0.3)               
+    return reg_column
 
 def cleanData(data):
     # Bring back Condition1/2, LowQualFinSF, 
@@ -26,7 +30,8 @@ def cleanData(data):
                      'GarageFinish', 'GarageCars', 'PavedDrive', 'Fence',
                      'MiscFeature', 'SaleType', 'LotConfig', 'Exterior1st',
                      'MasVnrArea', 'GarageType', 'SaleCondition', 'Id',
-                     'TotalBsmtSF', 'MiscVal', 'Utilities']
+                     'TotalBsmtSF', 'MSZoning', 'RoofMatl', 'Utilities',
+                     'MiscVal', 'HeatingQC', 'CentralAir', 'Functional']
     truncated_data = data.drop(drop_features, axis = 1, inplace = False)
     #Change all the degree'd categoricals into numbers
     seminumberedData = convertLeveledCats(truncated_data)
@@ -69,17 +74,17 @@ def convertLeveledCats(data):
                   'FireplaceQu': qualcond5,
                   'GarageQual': qualcond5,
                   'GarageCond': qualcond5,
-                  'HeatingQC': qualcond5,
+                  #'HeatingQC': qualcond5,
                   'PoolQC': qualcondPool,
                   'BsmtFinType1': qualcondbsmtfin,
                   'BsmtFinType2': qualcondbsmtfin,
-                  'LandSlope': qualcondland, 
-                  'CentralAir': {'Y': 1, 'N': 0}}, inplace = True)
+                  'LandSlope': qualcondland}, inplace = True) 
+                  #'CentralAir': {'Y': 1, 'N': 0}}, inplace = True)
     fillNA = ['ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond', 'KitchenQual', 
-              'FireplaceQu', 'GarageQual', 'GarageCond', 'HeatingQC', 'PoolQC', 
-              'BsmtFinType1', 'BsmtFinType2', 'LandSlope', 'CentralAir',
+              'FireplaceQu', 'GarageQual', 'GarageCond', 'PoolQC', 
+              'BsmtFinType1', 'BsmtFinType2', 'LandSlope',
                #following line added as bandaid for odd NaN entries 
-              'MSZoning', 'Neighborhood', 'RoofMatl', 'Foundation', 'Functional']
+               'Neighborhood', 'Foundation']
     #eliminate NAs leftover
     data[fillNA] = data[fillNA].fillna(value = 0)
     
@@ -95,37 +100,40 @@ def engineerFeatures(data):
     overall_adjust = regularize(data['OverallQual'] * data['OverallCond'])
     external_adjust = regularize(data['ExterQual'] * data['ExterCond'])
     bsmt1_adjust = regularize(data['BsmtQual'] * data['BsmtCond'] * 
-                              data['BsmtFinType1'])
+                              data['BsmtFinType1'], True)
     bsmt2_adjust = regularize(data['BsmtQual'] * data['BsmtCond'] * 
-                              data['BsmtFinType2'])
+                              data['BsmtFinType2'], True)
     garage_adjust = regularize(data['GarageQual'] * data['GarageCond'])
     data['PoolQC'] = regularize(data['PoolQC'])                                      
-
+    
     #CONSIDER ADDING LOWQUALFINSF with the lowest adjuster
     #instead of creating variable and then adding new column, add immediately
     ####there might be better way than hardcoding the weights
     data['alt_sqft'] = (data['LandSlope'] * 
                        (overall_adjust * (data['1stFlrSF'] + data['2ndFlrSF']) +
-                        0.2 * data['LowQualFinSF'] +
+                        #overall_adjust.quantile(0.2) * data['LowQualFinSF'] +
                         bsmt1_adjust * data['BsmtFinSF1'] + 
                         bsmt2_adjust * data['BsmtFinSF2'] +
-                        0.1 * data['BsmtUnfSF'] +
-                        garage_adjust * data['GarageArea'] + #maybe separate out
-                        0.1 * (data['LotArea'] - data['1stFlrSF'])))
+                        #bsmt1_adjust.quantile(0.1) * data['BsmtUnfSF'] +
+                        garage_adjust * data['GarageArea']))  #maybe separate out
+    
     data['alt_bathrooms'] = (data['BsmtFullBath'] + data['BsmtHalfBath'] + 
                              data['FullBath'] + data['HalfBath'])
     data['alt_kitchens'] = data['KitchenQual'] * data['KitchenAbvGr']
     data['alt_fireplaces'] = data['Fireplaces'] * data['FireplaceQu'] #maybe cut
-    data['outdoors_sqft'] = (data['WoodDeckSF'] + data['OpenPorchSF'] + 
-                             data['EnclosedPorch'] + data['3SsnPorch'] + 
-                             data['ScreenPorch'] + 
-                             (data['PoolArea'] * data['PoolQC']))
+    data['dev_outdoors_sqft'] = (data['WoodDeckSF'] + data['OpenPorchSF'] + 
+                                 data['EnclosedPorch'] + data['3SsnPorch'] + 
+                                 data['ScreenPorch']) 
+    data['Pool'] = data['PoolArea'] * data['PoolQC']
+    data['outdoor_sqft'] = (data['LotArea'] - data['1stFlrSF'] - 
+                            data['dev_outdoors_sqft'])                     
     #data['pool_sqft'] = data['PoolArea'] * data['PoolQC'] add back later?
     data['date_sold'] = 365.25 * data['YrSold'] + 30.44 * data['MoSold']
     data['date_sold'] = data['date_sold'] - min(data['date_sold'])
+    data['YearRemodAdd'] = data['YearRemodAdd'] - min(data['YearRemodAdd']) 
     #drop old ones
-    used_features = ['LandSlope', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF',
-                     'BsmtUnfSF', 'BsmtFinSF1', 'BsmtFinSF2', 'GarageArea',
+    used_features = ['LandSlope', '1stFlrSF', '2ndFlrSF',
+                     'BsmtFinSF1', 'BsmtFinSF2', 'GarageArea',
                      'LotArea', '1stFlrSF', 'BsmtFullBath', 'BsmtHalfBath',
                      'FullBath', 'HalfBath', 'KitchenQual', 'KitchenAbvGr',
                      'Fireplaces', 'FireplaceQu', 'WoodDeckSF', 'OpenPorchSF',
@@ -133,12 +141,13 @@ def engineerFeatures(data):
                      'PoolQC', 'YrSold', 'MoSold', 'OverallCond',
                      'OverallQual', 'ExterQual', 'ExterCond', 'BsmtQual',
                      'BsmtCond', 'BsmtFinType1', 'BsmtFinType2', 'GarageQual',
-                     'GarageCond']
+                     'GarageCond', 'date_sold', 'Pool', 'LowQualFinSF'] #starting to get rid of eng feat
     truncated_data = data.drop(used_features, axis = 1, inplace = False)
 
     return truncated_data
 
 clean_data = cleanData(raw_data)
+print clean_data
 y = clean_data['SalePrice']
 clean_data.drop('SalePrice', axis = 1, inplace = True)
 X = clean_data
@@ -147,18 +156,20 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     random_state = 42)
 
 ###### CatBoost ######
-cat_features_labels = ['Functional', 'Foundation', 'RoofMatl', 
-                       'Neighborhood', 'MSZoning'] 
+cat_features_labels = ['Foundation', 
+                       'Neighborhood'] 
 cat_features = []
 for feature in cat_features_labels:
     cat_features.append(X_train.columns.get_loc(feature))
 train_pool = Pool(X_train, y_train, cat_features = cat_features)
 test_pool = Pool(X_test, cat_features = cat_features)
-model = CatBoostRegressor(iterations = 500, depth = 5, learning_rate = 0.1,
-                          loss_function = 'RMSE')
+model = CatBoostRegressor(iterations = 1000, depth = 8, learning_rate = 0.05,
+                          loss_function = 'RMSE', logging_level = 'Silent',
+                          random_seed = 42)
 model.fit(train_pool)
 pred = model.predict(test_pool)
-print mean_squared_error(y_test, pred)
+print math.sqrt(mean_squared_error(y_test, pred))
+print model.get_feature_importance(train_pool)
 
 ##### Submission #######
 
