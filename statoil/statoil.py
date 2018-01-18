ba53ec100f8d93a4f484514e5a3f69261ed8357d
HadiKnown <yeshar.hadi@gmail.com>
2018-01-18 14:15:10 -0800

    Stacked the HH and HV outputs to be two separate channels for each pixel.

--- statoil/statoil.py
+++ statoil/statoil.py
@@ -16,6 +16,7 @@ def get_input_fn(data, num_epochs = None, shuffle = True):
              y = pd.Series(data['is_iceberg'].values),
              num_epochs = num_epochs,
              shuffle = shuffle) 
+
 def my_model(features, labels, mode, params):
     net = tf.feature_column.input_layer(features, params['feature_columns'])
     #play with specifying the channel number (which I think is the depth of the layr)
@@ -30,9 +31,8 @@ def main(unused_argv, use_angles = False):
             if raw['inc_angle'].iloc[i] == 'na': inc_angle_na.append(i) 
         raw.drop(labels = inc_angle_na, axis = 0, inplace = True)
     else: raw.pop('inc_angle')
- 
     #investigate why the following line works!
-    get_intensities = pd.DataFrame(raw['band_1'].values.tolist())
+    """get_intensities = pd.DataFrame(raw['band_1'].values.tolist())
     band_columns = {i: ('band_1_' + str(i)) 
                          for i in range(0, get_intensities.shape[1])}
     get_intensities.rename(columns = band_columns, inplace = True)
@@ -44,11 +44,20 @@ def main(unused_argv, use_angles = False):
 
     raw.drop(['band_1', 'band_2'], axis = 1, inplace = True)
     data = pd.concat([raw, get_intensities, get_intensities_2],
-                    axis = 1, join = 'inner')
+                    axis = 1, join = 'inner')"""
+    #could cause issues if I need lists instead of tuples
+    #also lazy to add column to raws... but I guess... whatever
+    bands = []
+    band_1 = raw.pop('band_1')
+    band_2 = raw.pop('band_2')
+    for i in range(raw.shape[0]):
+        bands.append(list(zip(band_1[i], band_2[i])))
+    data = pd.concat([raw, pd.DataFrame(bands)], axis = 1, join = 'inner') 
+
     #could just use train test split
     testing = data.iloc[int(0.8 * len(data)):]
     training = data.iloc[:int(0.8 * len(data))]
-
+  
     FEATURES = list(data)
     LABEL = 'is_iceberg'
 
